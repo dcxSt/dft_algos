39f8bbd8f03627d0e011c14e16ca9faced8f0b1f
Stephen Fay <dcxstephen@live.co.uk>
2023-06-28 13:27:15 -0400

    integer fft python binding works!

--- fftrs/src/lib.rs
+++ fftrs/src/lib.rs
@@ -3,10 +3,12 @@ pub mod constants;
 pub mod intfft;
 //pub mod iomod;
 
+use complex::Complex;
+use intfft::fft_quantized;
 //use pyo3::prelude::*;
 use pyo3::{pymodule, types::PyModule, PyResult, Python};
-use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn};
-use numpy::ndarray::{ArrayD, ArrayViewD}; //, ArrayViewMutD};
+use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn, PyArray};
+use numpy::ndarray::{ArrayD, ArrayViewD, Ix1}; //, ArrayViewMutD};
 
 //use complex::Complex;
 //use constants::{QUART_WAV, SINE};
@@ -42,6 +44,36 @@ fn integer_fft(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         let y = y.as_array();
         let z = dummy_add_arrays(x, y);
         z.into_pyarray(py)
+    }
+    #[pyfn(m)]
+    #[pyo3(name = "integer_fft_2048")]
+    fn integer_fft_2048<'py>(
+        py: Python<'py>,
+        xre: PyReadonlyArrayDyn<i64>,
+        xim: PyReadonlyArrayDyn<i64>,
+        ) -> &'py PyArray<i64, Ix1> {
+        let xre = xre.as_array();
+        let xim = xim.as_array();
+        assert_eq!(xre.len(), 2048);
+        assert_eq!(xim.len(), 2048);
+        // initiate input arrays
+        let mut flip: [Complex; 2048] = [Complex::new(0, 0); 2048];
+        let mut flop: [Complex; 2048] = [Complex::new(0, 0); 2048];
+        // Fill flip with data
+        for i in 0..2048 {
+            flip[i] = Complex::new(xre[i], xim[i]);
+        }
+        fft_quantized(&mut flip, &mut flop, 16, 18);
+        // Now flop has fft'd data
+        // Read data back into two i64 arrays
+        let mut outre = Vec::<i64>::new();
+        let mut outim = Vec::<i64>::new();
+        for i in 0..2048 {
+            outre.push(flop[i].re);
+            outim.push(flop[i].im);
+        }
+        let outre_pyarray = PyArray::from_vec(py, outre);
+        return outre_pyarray;
     }
 //     /// Python wrapper for integer FFT
 //     #[pyfn(m)]
